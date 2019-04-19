9d41b02987e44970f0511eec2be2e7bf69b13b42
宋神宗 <i@songshenzong.com>
2019-04-19 09:36:41 +0800

    Supported Sas, Ivision. (#104)

---
+++ Ivision.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace AlibabaCloud\Ivision;
+
+use AlibabaCloud\VersionResolverTrait;
+
+/**
+ * Resolve version based on the method name.
+ *
+ * @method V20190308\IvisionApiResolver v20190308()
+ */
+class Ivision
+{
+    use VersionResolverTrait;
+}
